76a919bfa9305f4c617ef92eabe20c98a40812cf
Chandni Kulshrestha <ckz2011@gmail.com>
2022-10-23 10:50:15 +0530

    Added Test Case 1

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,21 +5,21 @@ from joblib import load
 
 sys.path.append(".")
 
-# from utils import get_all_h_param_comb, tune_and_save
-import utils
+from utils import get_all_h_param_comb, tune_and_save
+# import utils
 from sklearn import svm, metrics
 
 # test case to check if all the combinations of the hyper parameters are indeed getting created
-def test_get_h_param_comb():
-    gamma_list = [0.01, 0.005, 0.001, 0.0005, 0.0001]
-    c_list = [0.1, 0.2, 0.5, 0.7, 1, 2, 5, 7, 10]
+# def test_get_h_param_comb():
+#     gamma_list = [0.01, 0.005, 0.001, 0.0005, 0.0001]
+#     c_list = [0.1, 0.2, 0.5, 0.7, 1, 2, 5, 7, 10]
 
-    params = {}
-    params["gamma"] = gamma_list
-    params["C"] = c_list
-    h_param_comb = get_all_h_param_comb(params)
+#     params = {}
+#     params["gamma"] = gamma_list
+#     params["C"] = c_list
+#     h_param_comb = get_all_h_param_comb(params)
 
-    assert len(h_param_comb) == len(gamma_list) * len(c_list)
+#     assert len(h_param_comb) == len(gamma_list) * len(c_list)
 
 def helper_h_params():
     # small number of h params
@@ -41,7 +41,24 @@ def helper_create_bin_data(n=100, d=7):
 
     return x_train, y_train
 
-def test_tune_and_save():    
+# def test_tune_and_save():    
+#     h_param_comb = helper_h_params()
+#     x_train, y_train = helper_create_bin_data(n=100, d=7)
+#     x_dev, y_dev = x_train, y_train
+
+#     clf = svm.SVC()
+#     metric = metrics.accuracy_score
+    
+#     model_path = "test_run_model_path.joblib"
+#     actual_model_path = tune_and_save(clf, x_train, y_train, x_dev, y_dev, metric, h_param_comb, model_path)
+
+#     assert actual_model_path == model_path
+#     assert os.path.exists(actual_model_path)
+#     assert type(load(actual_model_path)) == type(clf)
+    
+#     assert len(clf.predict(x_dev)) > 1 
+
+def test_quiz_case_1():    
     h_param_comb = helper_h_params()
     x_train, y_train = helper_create_bin_data(n=100, d=7)
     x_dev, y_dev = x_train, y_train
@@ -55,37 +72,22 @@ def test_tune_and_save():
     assert actual_model_path == model_path
     assert os.path.exists(actual_model_path)
     assert type(load(actual_model_path)) == type(clf)
+    
+    assert len(clf.predict(x_dev)) > 1 
 
-# what more test cases should be there
-# irrespective of the changes to the refactored code.
+def test_quiz_case_2():    
+    h_param_comb = helper_h_params()
+    x_train, y_train = helper_create_bin_data(n=100, d=7)
+    x_dev, y_dev = x_train, y_train
 
-# train/dev/test split functionality : input 200 samples, fraction is 70:15:15, then op should have 140:30:30 samples in each set
+    clf = svm.SVC()
+    metric = metrics.accuracy_score
+    
+    model_path = "test_run_model_path.joblib"
+    actual_model_path = tune_and_save(clf, x_train, y_train, x_dev, y_dev, metric, h_param_comb, model_path)
 
-
-# preprocessing gives ouput that is consumable by model
-
-# accuracy check. if acc(model) < threshold, then must not be pushed.
-
-# hardware requirement test cases are difficult to write.
-# what is possible: (model size in execution) < max_memory_you_support
-
-# latency: tik; model(input); tok == time passed < threshold
-# this is dependent on the execution environment (as close the actual prod/runtime environment)
-
-
-# model variance? --
-# bias vs variance in ML ?
-# std([model(train_1), model(train_2), ..., model(train_k)]) < threshold
-
-
-# Data set we can verify, if it as desired
-# dimensionality of the data --
-
-# Verify output size, say if you want output in certain way
-# assert len(prediction_y) == len(test_y)
-
-# model persistance?
-# train the model -- check perf -- write the model to disk
-# is the model loaded from the disk same as what we had written?
-# assert acc(loaded_model) == expected_acc
-# assert predictions (loaded_model) == expected_prediction
+    assert actual_model_path == model_path
+    assert os.path.exists(actual_model_path)
+    assert type(load(actual_model_path)) == type(clf)
+    
+    assert len(clf.predict(x_dev)) > 1 
