f94426ec374e64aa41eaafd194c7d14d28b0a199
Chandni Kulshrestha <ckz2011@gmail.com>
2022-11-13 21:58:16 +0530

    Added Predicted Values

--- api/app.py
+++ api/app.py
@@ -23,28 +23,41 @@ def hello():
 def upload_file():
     try:
         print("In Upload File")
-        if request.method == 'POST' and 'file' in request.files and request.files['file'].filename != '':
-          print("File Name is ")
-          print(request.files['file'].filename)
-          request.files['file'].save(os.path.join('uploaded_files', request.files['file'].filename))
+        if request.method == 'POST' and 'file1' in request.files and request.files['file1'].filename != '':
           
-          img = Image.open('uploaded_files/image.png')
+          print(request.files['file1'].filename)
+          print(request.files['file2'].filename)
+
+          request.files['file1'].save(os.path.join('uploaded_files', request.files['file1'].filename))
+          request.files['file2'].save(os.path.join('uploaded_files', request.files['file2'].filename))
+          
+          img1 = Image.open("uploaded_files/{}".format(request.files['file1'].filename))
+          img2 = Image.open("uploaded_files/{}".format(request.files['file2'].filename))
+
+          print("Images Fetched")
           
         #   img = img.reshape(8, 8)
-          img=img.resize((8,8))
+        #   img=img.resize((8,8))
+          img1 = img1.resize((8,8))
+          img2 = img2.resize((8,8))
           
         #   img = arr.reshape(8, 8)
-          img = np.array(img).reshape((-1, 64))
-          print("Arr")
-          print(img)
-        #   img = list(arr)
-          print(img)
+          img1 = np.array(img1).reshape((-1, 64))
+          img2 = np.array(img2).reshape((-1, 64))
+          
 
-          print("Image error")
+        #   img = list(arr)
         #   print(img)
 
-          predicted = model.predict(img)
-          return {"y_predicted":int(predicted[0])}
+          predicted1 = model.predict(img1)
+          predicted2 = model.predict(img2)
+
+          if int(predicted1[0]) == int(predicted2[0]):
+            return "Predicted Values are Same"
+          else:
+            return "Predicted Values are Not Same"
+
+        #   return {"y_predicted":int(predicted[0])}
         else:
           return "Use Method Post and Upload File by selecting form-data and key name as file with type as file"
     except Exception as e:
